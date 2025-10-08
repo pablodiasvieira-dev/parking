2f382526531450df6b46732b981389297c132961
pablodiasvieira-dev <pablovieira.dev@gmail.com>
2025-10-08 17:37:02 -0400

    verificando no git

--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -20,6 +20,8 @@ interface IModalDownUp {
     setExibirVaga: (value: boolean) => void
 }
 
+// TESTE ADICIONADO
+
 export const ModalDownUp = forwardRef<HTMLDivElement, IModalDownUp>(
     (
         { vagaLivreBloco, vagasTotaisBloco, isSelect, vagaSelecionada,
